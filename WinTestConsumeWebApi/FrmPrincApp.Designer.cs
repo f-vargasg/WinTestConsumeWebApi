b362fd31ca23c18ff3b39bf0519413d4bc12ef01
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-07-30 14:29:01 -0600

    Agregar archivos de proyecto.

---
+++ WinTestConsumeWebApi/FrmPrincApp.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace WinTestConsumeWebApi
+{
+    partial class FrmPrincApp
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butDo = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butDo
+            // 
+            this.butDo.Location = new System.Drawing.Point(12, 12);
+            this.butDo.Name = "butDo";
+            this.butDo.Size = new System.Drawing.Size(174, 41);
+            this.butDo.TabIndex = 0;
+            this.butDo.Text = "Execute";
+            this.butDo.UseVisualStyleBackColor = true;
+            this.butDo.Click += new System.EventHandler(this.butDo_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 86);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(985, 464);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // FrmPrincApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.butDo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmPrincApp";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butDo;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ WinTestConsumeWebApi/FrmPrincApp.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTestConsumeWebApi
+{
+    public partial class FrmPrincApp : Form
+    {
+        public FrmPrincApp()
+        {
+            InitializeComponent();
+        }
+
+        private void butDo_Click(object sender, EventArgs e)
+        {
+            string url = "";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+                
+        }
+    }
+}
